621a88efa07aca2699f281756c2cf3dda742d68d
MansoonM <136214687+MansoonM@users.noreply.github.com>
2024-01-29 12:11:11 +0530

    Add files via upload

---
+++ Responsive NavBar/main.js
@@ -0,0 +1,8 @@
+function toggleMenu() {
+  let navLinks = document.getElementById("navLinks");
+  if (navLinks.style.display === "block") {
+    navLinks.style.display = "none";
+  } else {
+    navLinks.style.display = "block";
+  }
+}
